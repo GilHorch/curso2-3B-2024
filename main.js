8695725eee1357d46a26c8400f7bd8643fb7b9b7
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-08-14 11:54:43 -0300

    14

--- main.js
+++ main.js
@@ -7,8 +7,14 @@ numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
 //seleciona todas as clases .parametro-senha__botao
 const botoes=querySelectorAll('.parametro-senha__botao');
 
-botoes[0].onclick = diminuiTamanho;
-
-function iminuiTamanho(){
+botoes[0].onclick = diminuiTamanho;//função para diminuir o tamanho da senha
+function diminuiTamanho(){
 tamanhoSenha=tamanhoSenha-1;//iminui uma unidade a variável tamanhoSenha
+numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
+}
+
+botoes[1].onclick = aumentaTamanho;//função para diminuir o tamanho da senha
+function aumentaTamanho(){
+tamanhoSenha=tamanhoSenha+1;//iminui uma unidade a variável tamanhoSenha
+numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
 }
