572a54005b10941a730d352d7a3ee13dcbff32d8
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2024-08-14 12:00:01 -0300

    1

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 //seleciona a clase farametro senha e armazena na variavel numero senha
 const numerosenha = document.querySelector('.parametro-senha__texto');
 
-let tamanhoSenha=12;
+let tamanhoSenha = 12;
 
 numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
 //seleciona todas as clases .parametro-senha__botao
-const botoes=querySelectorAll('.parametro-senha__botao');
+const botoes = querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;//função para diminuir o tamanho da senha
-function diminuiTamanho(){
-tamanhoSenha=tamanhoSenha-1;//iminui uma unidade a variável tamanhoSenha
-numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
+function diminuiTamanho() {
+    tamanhoSenha = tamanhoSenha - 1;//iminui uma unidade a variável tamanhoSenha
+    numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
 }
 
 botoes[1].onclick = aumentaTamanho;//função para diminuir o tamanho da senha
-function aumentaTamanho(){
-tamanhoSenha=tamanhoSenha+1;//iminui uma unidade a variável tamanhoSenha
-numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
+function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha + 1;//iminui uma unidade a variável tamanhoSenha
+    numerosenha.textContent = tamanhoSenha;//altera o valor do numero senha
 }
